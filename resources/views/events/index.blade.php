18e2c3df98ed81a359ed6d2fe88d22a2b78b278f
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-05-05 16:32:33 -0500

    created events folder for views. redefined routes

---
+++ resources/views/events/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+<h1>Index of Events</h1>
+<h3>All events listed in table style</h3>
+<ul>
+	<li>Delete event</li>
+	<li>Quick add</li>
+	<li>Edit</li>
+</ul>
+	<h4>Categories >> My Events || Group Events</h4>
+	<h4>Sorting: Day | Week | Month</h4>
+@stop
