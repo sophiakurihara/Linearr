c7dad1efc4dba61e5039d164f2f8612c61b8e588
Phillip <ppalatas@gmail.com>
2017-05-05 16:41:53 -0500

    Changed color shcemes.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,11 +4,15 @@
    <title>Linearr</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
     <link rel="stylesheet" href="/css/head.css">
+    @yield ('additional_css')
 </head>
 <body>
+	<div class="navBar">
+		<div class="logo"><h1>LOGO</h1></div>
+	</div>
 
-	<h1 class="logo">LOGO</h1>
 	<!-- place navbar here in master -->
    @yield('content')
 </body>
