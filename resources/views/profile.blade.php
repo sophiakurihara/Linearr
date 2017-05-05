42ac0a42fff03da8b792051ff3eff82de71aa386
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-05-05 15:08:00 -0500

    created home view created profile view head.css >> will include navbar css

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+<h1>This is the user's home page</h1>
+	<h3>Calendar would populate here</h3>
+	<h3>Add side nav-bar</h3>
+	<h3>Add user info on right</h3>
+@stop
