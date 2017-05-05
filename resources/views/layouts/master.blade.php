7d06066a5c512d7d83c80b87be8bca9dd1d64a91
Phillip <ppalatas@gmail.com>
2017-05-05 15:14:38 -0500

    dont merge -- commiting to be able to pull and merge from master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <title>Linearr</title>
-    <!-- Latest compiled and minified CSS -->
-	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-	<link rel="stylesheet" href="/linearr.css">
-</head>
-<body>
-	@include('partials.navbar')
-    @yield('content')
-=======
    <title>Linearr</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
@@ -19,6 +9,5 @@
 <body>
 	<!-- place navbar here in master -->
    @yield('content')
->>>>>>> master
 </body>
 </html>
