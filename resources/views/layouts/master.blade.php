b2ed1986205c3220eba7dd9e0664ec744ef6c273
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-05-08 11:33:37 -0500

    styling the home page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,12 +3,13 @@
 <head>
    <title>Linearr</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
     <link rel="stylesheet" href="/css/head.css">
     @yield ('additional_css')
 </head>
 <body>
 	<div class="logo">
-		<div class="logo"><h1>Linearr</h1></div>
+		<div class="logo"><h1>LINEARR</h1></div>
 	</div>
 
 	<!-- place navbar here in master -->
