3cec8c057f98a6cfda5955a26239e4ef2d0c869b
Cameron Palatas <cameronlewispalatas@gmail.com>
2017-05-08 16:11:01 -0500

    Revert

--- app/Http/Controllers/usersController.php
+++ app/Http/Controllers/usersController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests;
 use App\User;
 use Hash;
 
