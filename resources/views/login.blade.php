1132057df9acab74c4a32ea4b3738fe7ede215c6
Cameron Palatas <cameronlewispalatas@gmail.com>
2017-05-08 14:54:55 -0500

    Css fixes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,8 +12,8 @@
     <img src="/img/giphy.gif">
 
     <div class="banner-buttons">
-        <a href="login" id="login">Login</a>
-        <a href="register" id="register">Register</a>
+        <a href="#login" id="login">Login</a>
+        <a href="#register" id="register">Register</a>
     </div>
 
     <div class="banner-right-register hide">
@@ -23,13 +23,11 @@
         <form action=" {{ action('UsersController@registerUser') }}" method="POST">
             
             {{ csrf_field() }}
-            <label for="firstname">First Name:</label><input type="text" name="first_name" id="firstname" placeholder="{{ $errors->has('firstname') ? 'Please enter your first name' : 'First name' }}" value="{{ $errors->has('firstname') ? '' : old('firstname') }}">
-            <label for="lastname">Last Name:</label><input type="text" name="last_name" id="lastname" placeholder="{{ $errors->has('lastname') ? 'Please enter your last name' : 'Last name' }}" value="{{ $errors->has('lastname') ? '' : old('lastname') }}">
-            <label for="email">E-Mail:</label><input type="text" name="email" id="email" placeholder="{{ $errors->has('email') ? 'Please enter your email address' : 'Email' }}" value="{{ $errors->has('email') ? '' : old('email') }}">
-            <label for="phone">Phone:</label><input type="text" name="phone" id="phone" placeholder="{{ $errors->has('phone') ? 'Please enter your phone number' : 'Phone' }}" value="{{ $errors->has('phone') ? '' : old('phone') }}">
-            <label for="password">Password:</label><input type="text" name="password" id="password" placeholder="{{ $errors->has('password') ? 'Please enter your password' : 'Password' }}">
-            {!! method_field('POST') !!}
-
+            <label for="firstname">First Name:</label><input type="text" name="first_name" id="firstname">
+            <label for="lastname">Last Name:</label><input type="text" name="last_name" id="lastname">
+            <label for="email">E-Mail:</label><input type="text" name="email" id="email">
+            <label for="phone">Phone:</label><input type="text" name="phone" id="phone">
+            
             <button type="submit" class="login-button">Register</button>
 
         </form>
@@ -48,39 +46,20 @@
 
 </div>
 
-<?php
-
-if(!empty($errors)) {
-    $string = "";
-    foreach($errors as $error) {
-        $string .= "$error, ";
-    }
-}
-
 <script src="/js/jquery/10.8.3/jquery.min.js" type="text/javascript"></script>
 <script src="/js/jquery/jquery-3.2.0.min.js" type="text/javascript"></script>
 
 <script>
 
 $(document).ready(function(){
-    var errors = [];
-
-    if() {
-
-    }
-
-
-
-    $("#login").click(function(e){
-        e.preventDefault();
+    $("#login").click(function(){
         $(".banner-buttons").fadeOut(400);
         setTimeout(function(){
             $(".banner-right-login").removeClass("hide");
             $(".banner-right-login").fadeIn(400);
         }, 440);
     });
-    $("#register").click(function(e){
-        e.preventDefault();        
+    $("#register").click(function(){
         $(".banner-buttons").fadeOut(400);
         setTimeout(function(){
             $(".banner-right-register").removeClass("hide");
@@ -93,5 +72,3 @@ $(document).ready(function(){
 
 
 @stop
-
-
