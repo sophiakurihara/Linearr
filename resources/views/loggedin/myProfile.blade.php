3153bf14d08b6836e84c681cd4bc9627371698e5
Phillip <ppalatas@gmail.com>
2017-05-10 14:19:38 -0500

    Created basic layout for the profile page. Added jQuery to open a popup class when the date is clicked 	-The pop up window needs to be populated with data from event table.

--- resources/views/loggedin/myProfile.blade.php
+++ resources/views/loggedin/myProfile.blade.php
@@ -7,73 +7,100 @@
 @stop
 
 @section('loggedin_content')
-
 <div class="userLeftControllPanel">
-	<div class="userLeftControllPanel-sub-sections firstControlPanel-sub-section sub-section-text sub-section-text:hover">My Events</div>
-	<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover">Create Event</div>
-	<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover"> Contacts</div>
-	<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover">Edit Profile</div>
-	<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover"><a href="logout">Logout</a></div>
-	
-	<div class="settings"></div>
-</div>
-<div id="calendarContainer">
-	<div class="month"> 
-		<ul>
-		  <li class="prev">&#10094;</li>
-		  <li class="next">&#10095;</li>
-		  <li>
-		    August<br>
-		    <span style="font-size:18px">2016</span>
-		  </li>
-		</ul>
+		<div class="userLeftControllPanel-sub-sections firstControlPanel-sub-section sub-section-text sub-section-text:hover"><a href="myEvents">My Events</a></div>
+		<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover"><a href="createEvent">Create Events</a></div>
+		<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover"> <a href="contacts">Contacts</a></div>
+		<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover"><a href="editProfile">Edit Profile</a></div>
+		<div class="userLeftControllPanel-sub-sections sub-section-text sub-section-text:hover"><a href="logout">Logout</a></div>
+		
+		<div class="settings"></div>
 	</div>
+	<div id="calendarContainer">
+		<div class="month"> 
+			<ul>
+			  <li class="prev">&#10094;</li>
+			  <li class="next">&#10095;</li>
+			  <li>
+			    August<br>
+			    <span style="font-size:18px">2016</span>
+			  </li>
+			</ul>
+		</div>
 
-	<ul class="weekdays">
-	  <li>Mo</li>
-	  <li>Tu</li>
-	  <li>We</li>
-	  <li>Th</li>
-	  <li>Fr</li>
-	  <li>Sa</li>
-	  <li>Su</li>
-	</ul>
+		<ul class="weekdays">
+		  <li>Mo</li>
+		  <li>Tu</li>
+		  <li>We</li>
+		  <li>Th</li>
+		  <li>Fr</li>
+		  <li>Sa</li>
+		  <li>Su</li>
+		</ul>
 
-	<ul class="days"> 
-	  <li>1</li>
-	  <li>2</li>
-	  <li>3</li>
-	  <li>4</li>
-	  <li>5</li>
-	  <li>6</li>
-	  <li>7</li>
-	  <li>8</li>
-	  <li>9</li>
-	  <li><span class="active">10</span></li>
-	  <li>11</li>
-	  <li>12</li>
-	  <li>13</li>
-	  <li>14</li>
-	  <li>15</li>
-	  <li>16</li>
-	  <li>17</li>
-	  <li>18</li>
-	  <li>19</li>
-	  <li>20</li>
-	  <li>21</li>
-	  <li>22</li>
-	  <li>23</li>
-	  <li>24</li>
-	  <li>25</li>
-	  <li>26</li>
-	  <li>27</li>
-	  <li>28</li>
-	  <li>29</li>
-	  <li>30</li>
-	  <li>31</li>
-	</ul>
-</div>	
-
+		<ul class="days"> 
+		  <li class="myBtn1">1</li>
+		  <li class="myBtn2">2</li>
+		  <li class="myBtn3">3</li>
+		  <li class="myBtn4">4</li>
+		  <li class="myBtn5">5</li>
+		  <li class="myBtn6">6</li>
+		  <li class="myBtn7">7</li>
+		  <li class="myBtn8">8</li>
+		  <li class="myBtn9">9</li>
+		  <li class="myBtn10"><span class="active">10</span></li>
+		  <li class="myBtn11">11</li>
+		  <li class="myBtn12">12</li>
+		  <li class="myBtn13">13</li>
+		  <li class="myBtn14">14</li>
+		  <li class="myBtn15">15</li>
+		  <li class="myBtn16">16</li>
+		  <li class="myBtn17">17</li>
+		  <li class="myBtn18">18</li>
+		  <li class="myBtn19">19</li>
+		  <li class="myBtn20">20</li>
+		  <li class="myBtn21">21</li>
+		  <li class="myBtn22">22</li>
+		  <li class="myBtn23">23</li>
+		  <li class="myBtn24">24</li>
+		  <li class="myBtn25">25</li>
+		  <li class="myBtn26">26</li>
+		  <li class="myBtn27">27</li>
+		  <li class="myBtn28">28</li>
+		  <li class="myBtn29">29</li>
+		  <li class="myBtn30">30</li>
+		  <li class="myBtn31">31</li>
+		</ul>
+	</div>	
+	<!-- The Modal -->
+	<div id="myModal" class="modal">
+		<!-- Modal content -->
+		<div class="modal-content">
+			<span class="close">&times;</span>
+			<p>Event Title<br>
+			Event Date<br>
+			Event Time <br>
+			Number of People Attending <br>
+			Invite Contacts
+			</p>
+		</div>
+	</div>
+	<script src="/js/jquery/10.8.3/jquery.min.js" type="text/javascript"></script>
+	<script src="/js/jquery/jquery-3.2.0.min.js" type="text/javascript"></script>
+	<script>
+		$(document).ready(function(){
+			for(let i = 0; i <= 31; i++)
+			{
+				$(".myBtn" + i).click(function(){
+					console.log("you clicked" + i);
+					$("#myModal").css("display", "block");
+				});
+				$(".close").click(function(){
+					$("#myModal").css("display", "none");
+				});
+			}
+		});
+	</script>
 @stop
 
 
