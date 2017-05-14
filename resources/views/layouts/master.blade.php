b4aa74e0cc025906f68e91333c7dc006edca6b91
Phillip <ppalatas@gmail.com>
2017-05-14 14:56:01 -0500

    merging master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <link rel="stylesheet" href="/css/head.css">
         <link rel="stylesheet" href="/css/footer.css">
         <meta charset="utf-8">
@@ -24,13 +25,15 @@
 
   @include('partials.below_banner')
 
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <!-- Bootstrap JS -->
   <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
   <!-- jQuery JS -->
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+  <!-- <script src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
+  crossorigin="anonymous"></script> -->
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
 </body>
 </html>
