4b162e28c7f2d4ddf1ec7c9e9868e684b6ce4b69
Phillip <ppalatas@gmail.com>
2017-05-15 18:12:30 -0500

    Forms completed for settings
    	-- need to do the Profile picture and add email and password to settings/edit page.

--- resources/views/loggedin/settings.blade.php
+++ resources/views/loggedin/settings.blade.php
@@ -22,7 +22,7 @@
 							<div class="editIcon" id="profilePictureEdit"></div>
 						Profile Picture</a>
 						
-							<button type="submit" class="uploadProfilePic">Upload Picture</button>
+							<button type="submit" name="editPictureButton" class="uploadProfilePic">Upload Picture</button>
 					
 					</div>
 
@@ -31,35 +31,54 @@
 							<input type="file" name="uploadPic" class="uploadProfilePic ">
 						</form> -->
 					<div class="editingRows firstName">
-						<a id="firstNameEdit">
-							<div class="editIcon"></div>
-							First Name
-						</a>
-						<button type="submit" class="saveFirstNameButton">save</button>
-						<input type="text" class="editFirstName">
+						<!-- chosing post method to populate... Choosing what controller function we need to access for the form to work -->
+						<form method="POST" action="{{ action('UsersController@editAccount') }}">
+						{!! csrf_field() !!}
+						<!-- PUT is for updating as to originally POSTing something to the DB -->
+						{{ method_field('PUT')}} 
+
+							<a id="firstNameEdit">
+								<div class="editIcon"></div>
+								First Name
+							</a>
+							<button type="submit" name="firstNameButton" class="saveFirstNameButton">save</button>
+							<input type="text" name="first_name" class="editFirstName">
+						</form>
 					</div>
-					
+
 					<div class="editingRows lastName">
-						<a id="lastNameEdit">
-							<div class="editIcon"></div>
-							Last Name
-						</a>
-						<button type="submit" class="saveLastNameButton">save</button>
-						<input type="text" class="editLastName">
+						<!-- chosing post method to populate... Choosing what controller function we need to access for the form to work -->
+						<form method="POST" action="{{ action('UsersController@editAccount') }}">
+						{!! csrf_field() !!}
+						<!-- updates information -->
+						{{ method_field('PUT') }} 
+
+							<a id="lastNameEdit">
+								<div class="editIcon"></div>
+								Last Name
+							</a>
+							<button type="submit" name="lastNameButton" class="saveLastNameButton">save</button>
+							<input type="text" name="last_name" class="editLastName">
+						</form>
 					</div>
 
 					<div class="editingRows phoneNumber">
-						<a id="phoneNumberEdit">
-							<div class="editIcon"></div>
-							Phone Number
-						</a>
-						<button type="submit" class="savePhoneNumberButton">save</button>
-						<input type="text" class="editPhoneNumber">
+						<!-- chosing post method to populate... Choosing what controller function we need to access for the form to work -->
+						<form method="POST" action="{{ action('UsersController@editAccount') }}">
+						{!! csrf_field() !!} 
+						{{ method_field('PUT') }}
+
+							<a id="phoneNumberEdit">
+								<div class="editIcon"></div>
+								Phone Number
+							</a>
+							<!-- creating a name to be referenced in the controller's function -->
+							<button type="submit" name="phoneNumberButton" class="savePhoneNumberButton">save</button>
+							<input type="text" name="phone" class="editPhoneNumber">
+						</form>
 					</div>
 				</tr>
 			</tbody>
-
-
 	</div>	
 
 @stop
@@ -74,22 +93,21 @@
 
 		$("#firstNameEdit").click(function(){
 			$(".editFirstName").fadeIn(500);
-			$("#settingsMainContainer > div.editingRows.firstName > button").fadeIn(500);
+			$("#settingsMainContainer > div.editingRows.firstName > form > button").fadeIn(500);
 			console.log('You are trying to edit your first name');
 		});
 
 		$("#lastNameEdit").click(function(){
 			$(".editLastName").fadeIn(500);
-			$("#settingsMainContainer > div.editingRows.lastName > button").fadeIn(500);
+			$("#settingsMainContainer > div.editingRows.lastName > form > button").fadeIn(500);
 			console.log('You are trying to edit your first name');
 		});
 
 		$("#phoneNumberEdit").click(function(){
 			$(".editPhoneNumber").fadeIn(500);
-			$("#settingsMainContainer > div.editingRows.phoneNumber > button").fadeIn(500);
+			$("#settingsMainContainer > div.editingRows.phoneNumber > form > button").fadeIn(500);
 			console.log('You are trying to edit your first name');
 		});
-
 
 
 	});
