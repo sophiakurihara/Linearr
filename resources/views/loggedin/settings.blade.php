0ff0fc93315c50eb67722f574c9c7fabc38baf79
Phillip <ppalatas@gmail.com>
2017-05-15 14:13:37 -0500

    dont pull or merge

--- resources/views/loggedin/settings.blade.php
+++ resources/views/loggedin/settings.blade.php
@@ -15,40 +15,39 @@
             <h1>Settings</h1>
             <hr>
 
-			<!-- <table class="table-fill">
-			<thead>
-			<tr>
-				<th class="text-left" id="profilePicture">Profile Picture</th>
-				<th class="text-left" id="firstName">First Name</th>
-				<th class="text-left" id="lastName">Last Name</th>
-				<th class="text-left" id="phone">Phone</th>
-				<th class="text-left" id="email">Email</th>
-			</tr>
-			</thead> -->
-
 			<tbody class="table-hover">
-
 				<tr>
-					<div class="editingRows profilePicture"><div class="settingsIcon"></div>Profile Picture</div>
+					<div class="editingRows profilePicture">
+						<a id="profilePictureEdit">
+							<div class="editIcon" id="profilePictureEdit"></div>
+						</a>Profile Picture
+					</div>
+					<div class="editingRowsPicture profilePicture hide">
+						<input type="text" name="editProfPicture"></input>
+						<button type="submit">Save</button>
+						<div class="editIcon"></div>
+					</div>
 						<!-- Instead of text, insert the profile picture where the text is located .... upon click of the edit png/href a have pop up to choose from your desktop/finder etc.  -->
-					<div class="editingRows firstName"><div class="settingsIcon"></div>First Name</div>
-					<div class="editingRows lastName"><div class="settingsIcon"></div>Last Name</div>
-					<div class="editingRows phoneNumber"><div class="settingsIcon"></div>Phone Number</div>
+					<div class="editingRows firstName"><a id="firstNameEdit"><div class="editIcon"></div></a>First Name</div>
+					<div class="editingRows lastName"><a id="lastNameEdit"><div class="editIcon"></div></a>Name</div>
+					<div class="editingRows phoneNumber"><a id="firstNameEdit"><div class="editIcon"></div></a>Phone Number</div>
 				</tr>
-
 			</tbody>
-			</table>
+
+
 	</div>	
 
 @stop
 
 @section('js')
 <script>
-	$("tr").hover(function(){
-	    $(this).css("background-color", "yellow");
-	    }, function(){
-	    $(this).css("background-color", "pink");
-	});	
+	$(document).ready(function(){
 
-
+		$("#profilePictureEdit").click(function(){
+		    $(this).removeClass("profilePicture");
+		    console.log('You clicked the edit button.');
+		    // $(this).removeClass("profilePicture");
+		    $(this).addClass("editingRowsPicture");
+		});	
+	});
 </script>
